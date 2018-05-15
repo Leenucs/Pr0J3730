12eb02b2c318bca0794fd3cae2ddb205dffac7c7
Matt <matthias.david45@gmail.com>
2018-05-15 20:37:40 +0200

    Ajout pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { IntroPage } from '../pages/intro/intro';
+import { Page1Page } from '../pages/page1/page1';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    IntroPage,
+    Page1Page
   ],
   imports: [
     BrowserModule,
@@ -19,7 +23,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    IntroPage,
+    Page1Page
   ],
   providers: [
     StatusBar,
--- src/pages/intro/intro.ts
+++ src/pages/intro/intro.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Page1Page } from '../page1/page1';
 
 @Component({
   selector: 'page-intro',
@@ -9,6 +10,10 @@ export class IntroPage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  openPage() {
+    this.navCtrl.push(Page1Page)
   }
 
 }
